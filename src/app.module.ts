7f15b45ce336ad57eae5dbbfaff29624b44b586e
kevinbatard <kevin.batard4064@gmail.com>
2023-02-16 10:53:59 +0100

    feat add swagger, adaptation du main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
