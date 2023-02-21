d2a4d5f37fd922ae20ae0a63ff85f961511741a9
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-02-21 13:49:31 +0100

    Feat Table article

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { LanguagesService } from 'src/languages/languages.service';
+import { CategoriesService } from 'src/categories/categories.service';
+import { FrameworksService } from 'src/frameworks/frameworks.service';
 
 @Module({
   controllers: [ArticlesController],
-  providers: [ArticlesService]
+  providers: [ArticlesService, LanguagesService, CategoriesService, FrameworksService]
 })
 export class ArticlesModule {}
