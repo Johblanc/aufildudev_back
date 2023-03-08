bbc3c556f2c33f7b270f6720b340f12c52938450
kevinbatard <kevin.batard4064@gmail.com>
2023-03-08 13:36:58 +0100

    fix data renvoyé au create comment

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -31,6 +31,7 @@ export class CommentsService {
         select: {
           id: true,
           content: true,
+          created_at: true,
           article: { id: true, title: true },
           user: { id: true, pseudo: true },
         },
