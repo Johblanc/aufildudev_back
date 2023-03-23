a89c2c7e37bd7902b5a05e76f9adcf2d4bcd3590
kevinbatard <kevin.batard4064@gmail.com>
2023-03-23 09:19:48 +0100

    fix changement de l'update pour seulement email

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,7 +2,8 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 import { OmitType } from '@nestjs/mapped-types';
 
-
-export class UpdateUserDto extends PartialType(OmitType( CreateUserDto,["pseudo"])) {} /* OmitType = on ne veut pas update le pseudo */ 
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['pseudo', 'password']),
+) {} /* OmitType = on ne veut pas update le pseudo */
 
 /* PartialType prend toute la CreateUserDto et met le tout en optionnel pour UpdateUserDto = Raccourci*/
